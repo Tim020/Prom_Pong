482062524137994c9f4d453483ec656435979d30
Robin Stephenson <robin.mascros@gmail.com>
2016-04-15 17:22:25 +0100

    Created init in ButtonListener class

--- game/services.py
+++ game/services.py
@@ -10,3 +10,20 @@ class ANSIEscape:
     @staticmethod
     def set_graphics(attr, fore, back):
         pass
+
+
+class ButtonListener:
+    getter = None
+    cb = None
+    debounce = True
+
+    def __init__(self, getter, cb, debounce=True):
+        """
+        Creates a new button listener
+        :param getter: a getter function for the button you want to watch
+        :param cb: a callback function to execute when the button is pressed
+        :param debounce: should the button listener perform a software debounce?
+        """
+        self.getter = getter
+        self.cb = cb
+        self.debounce = debounce
