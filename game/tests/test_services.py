ebf1a9d0a564d6857eda919d70e8832e88381a44
Robin Stephenson <robin.mascros@gmail.com>
2016-04-17 22:28:39 +0100

    Made some test fixes

--- game/tests/test_services.py
+++ game/tests/test_services.py
@@ -4,6 +4,7 @@ from game.services import ANSIEscape, ButtonListener
 
 escape = ANSIEscape()
 
+
 def _getter():
     return 0
 
@@ -24,8 +25,8 @@ class TestANSIEscape(TestCase):
         """
         .move_cursor should return the sequence for moving the cursor when it is given no args
         """
-        code = escape.move_cursor()
-        self.assertEqual(code, "\033[0;0H")
+        code = escape.set_cursor_position()
+        self.assertEqual(code, "\033[0;0f")
 
     def test_move_cursor_args(self):
         """
@@ -33,44 +34,21 @@ class TestANSIEscape(TestCase):
         """
         x = 12
         y = 8
-        code = escape.move_cursor(x, y)
-        self.assertEqual(code, "\033[{};{}H".format(y, x))
+        code = escape.set_cursor_position(x, y)
+        self.assertEqual(code, "\033[{};{}f".format(y, x))
 
     def test_move_cursor_illegal_args(self):
         """
         .move_cursor should raise an exception if the args given to it are illegal, eg. out of range
         """
         with self.assertRaises(ValueError):
-            escape.move_cursor(120, 90)
-            escape.move_cursor(-1, 0)
-            escape.move_cursor(0, 180)
+            escape.set_cursor_position(120, 90)
+            escape.set_cursor_position(-1, 0)
+            escape.set_cursor_position(0, 180)
 
         with self.assertRaises(TypeError):
-            escape.move_cursor(0.5, 2)
-            escape.move_cursor("0", "2")
-
-    def test_set_graphics(self):
-        """
-        .set_graphics should return the sequence to change the settings given in the args
-        """
-        attr = 8
-        fore = 30
-        back = 40
-        code = escape.set_graphics(attr, fore, back)
-        self.assertEqual(code, "\033[{};{};{}m".format(attr, fore, back))
-
-    def test_set_graphics_illegal_args(self):
-        """
-        .set_graphics should raise an exception if the args given to it are illegal
-        """
-        with self.assertRaises(ValueError):
-            escape.set_graphics(3, 30, 40)
-            escape.set_graphics(0, -4, 47)
-            escape.set_graphics(0, 33, 50)
-
-        with self.assertRaises(TypeError):
-            escape.set_graphics("0", "30", 47)
-            escape.set_graphics(0.0, 30.1, 47.0)
+            escape.set_cursor_position(0.5, 2)
+            escape.set_cursor_position("0", "2")
 
 
 class TestButtonListenerDebounce(TestCase):
@@ -84,3 +62,8 @@ class TestButtonListenerDebounce(TestCase):
 
 class TestButtonListenerNoDebounce(TestCase):
     button = ButtonListener(_getter, _callback, False)
+
+    def test_constructor(self):
+        self.assertEqual(self.button.cb(), _callback())
+        self.assertEqual(self.button._getter(), _getter())
+        self.assertFalse(self.button._debounce)
